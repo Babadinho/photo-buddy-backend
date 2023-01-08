b6f9dc620bc03c646fe1af282dcac57fb0111eaf
Donald Iloekwe <donytex4u@gmail.com>
2023-01-08 14:10:54 +0000

    add hashtags model + image upload

--- base/models.py
+++ base/models.py
@@ -6,15 +6,24 @@ from django.contrib.auth.models import User
 class Photo(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    # images =
-    category = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    hashtags = models.CharField(max_length=200, null=True, blank=True)
     caption = models.TextField(null=True, blank=True)
     likes = models.IntegerField(null=True, blank=True, default=0)
     comments = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self._id
+        return str(self.user)
+
+class Hashtag(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    hashtags = models.CharField(max_length=200, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.hashtags)
 
 class Comment(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
@@ -26,14 +35,13 @@ class Comment(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self._id
+        return str(self._id)
 
 class Like(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
-    photo = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self._id
+        return str(self._id)
