52db03d5e3ad5647fa2d30f056cce0c2d195d92b
Donald Iloekwe <donytex4u@gmail.com>
2023-01-08 13:27:26 +0000

    setup database and model data

--- base/models.py
+++ base/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Photo(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # images =
+    category = models.CharField(max_length=200, null=True, blank=True)
+    caption = models.TextField(null=True, blank=True)
+    likes = models.IntegerField(null=True, blank=True, default=0)
+    comments = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self._id
+
+class Comment(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    photo = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
+    likes = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self._id
+
+class Like(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    photo = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self._id
